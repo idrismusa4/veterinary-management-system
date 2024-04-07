da68546da7b9fa5fb996629c37869d1e955b6f96
Idris Musa <idrismusa004@gmail.com>
2024-04-07 23:07:04 +0100

    Update Django version to 4.2.1 in requirements.txt

--- webAppProject/settings.py
+++ webAppProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-e_1shz^m-b@k%7goekb6&zl@3pz%ge_ihn$2uhs((+#8br1_ym
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.netlify.app']
 
 
 # Application definition
