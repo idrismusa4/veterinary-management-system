fd7902927fce2f5c8f9b82dd49a2385b4062d239
Idris Musa <idrismusa004@gmail.com>
2024-05-06 16:25:25 +0100

    chore: Remove unnecessary dependencies

--- hospAdmin/models.py
+++ hospAdmin/models.py
@@ -21,11 +21,38 @@ class Appointment(models.Model):
         return f'{self.patient.user.username} - {self.date} - {self.time}'
 
 class Session(models.Model):
+    PRESCRIPTION_CHOICES = (
+        ('paracetamol, ibuprofen', 'Paracetamol, Ibuprofen'),
+        ('antibiotics', 'Antibiotics'),
+        ('antihistamines', 'Antihistamines'),
+        ('antacids', 'Antacids'),
+        ('anticoagulants', 'Anticoagulants'),
+        ('antidepressants', 'Antidepressants'),
+        ('antipsychotics', 'Antipsychotics'),
+        ('anesthetics', 'Anesthetics'),
+        ('antivirals', 'Antivirals'),
+        ('bronchodilators', 'Bronchodilators'),
+        ('contraceptives', 'Contraceptives'),
+        ('diuretics', 'Diuretics'),
+        ('laxatives', 'Laxatives'),
+        ('stimulants', 'Stimulants'),
+        ('steroids', 'Steroids'),
+        ('muscle relaxants', 'Muscle Relaxants'),
+        ('narcotics', 'Narcotics'),
+        ('sedatives', 'Sedatives'),
+        ('tranquilizers', 'Tranquilizers'),
+        ('vaccines', 'Vaccines'),
+        ('vitamins', 'Vitamins'),
+        ('other', 'Other'),
+)
+
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE, null=True)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     description = models.TextField()
+    prescription = models.CharField(max_length=100, choices=PRESCRIPTION_CHOICES, default='paracetamol, ibuprofen')
+
 
     def __str__(self):
         return f'{self.patient.user.username} - {self.date}'
